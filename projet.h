d0ee87f285d451f255cb32554301b68e5fecedd7
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-12-10 10:31:27 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
 
+
+typedef struct station {
+    int identifiant;
+    long capacite;
+    long som_conso;
+}Station; 
+
+typedef struct AVL{
+    Station * station;
+    struct AVL * fg;
+    struct AVL * fd;
+    int eq;
+}AVL;
+
+
+typedef AVL* pavl;
