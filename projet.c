9f6cac5c6df737b3bef4cc958e9c2b0e0510a2a2
zquaddy <63981756+zquaddy@users.noreply.github.com>
2024-12-14 20:38:18 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -27,6 +27,36 @@ typedef struct AVL{
 // Définition d'un type pavl pour un pointeur vers un arbre AVL
 typedef AVL* pavl;
 
+// Fonction pour renvoyer le maximum entre deux valeurs
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+// Fonction pour renvoyer le minimum entre deux valeurs
+int min(int a, int b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+// Fonction pour renvoyer le minimum parmi trois valeurs
+int min3(int a, int b, int c) {
+    int min_ab = min(a, b);  // Trouver le minimum entre `a` et `b`
+    return min(min_ab, c);   // Comparer ce minimum avec `c`
+}
+
+// Fonction pour renvoyer le maximum parmi trois valeurs
+int max3(int a, int b, int c) {
+    int max_ab = max(a, b);  // Trouver le maximum entre `a` et `b`
+    return max(max_ab, c);   // Comparer ce maximum avec `c`
+}
+
 // Fonction pour créer un arbre AVL avec une station donnée
 pavl CreerAvl(Station * station){
 pavl arbre =malloc(sizeof(Pavl); // Allocation dynamique pour un nouvel arbre AVL
