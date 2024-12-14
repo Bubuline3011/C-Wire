c6fcf0c840438d5911fe948eb6e8273ca4d5bcba
Bubuline3011 <167123750+Bubuline3011@users.noreply.github.com>
2024-12-14 17:01:58 +0100

    C-Wire.sh

--- C-Wire.sh
+++ C-Wire.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+#Fonction pour aider l'utilisateur
 aide(){
 	echo "Le script premet de filtrer les donneés d'un fichier, crée un fichier qui contient une liste avec toutes les stations et d'autres inforations (somme des consommateurs etc)"
 	echo " $0 <chemin_csv> <type_station> <type_conso> [<id_centrale>][-h]" # les crochets sont utilisés pour dire que largument est optionnel
 	echo "Paramètres :"
-    	echo "  <chemin_csv>       :  Chemin vers le fichier de données CSV (obligatoire)"
+    	echo "  <chemin_csv>       : Chemin vers le fichier de données CSV (obligatoire)"
     	echo "  <type_station>     : Type de station à traiter : hvb, hva ou lv (obligatoire)"
     	echo "  <type_conso>       : Type de consommateur à analyser : comp, indiv, all (obligatoire)"
-    	echo "Restrictions :"
-    	echo "   hvb  		   : seulement comp (entreprises)"
-    	echo "   hva 		   : seulement comp (entreprises)"
+    	echo "Type de consommateurs :"
+    	echo "   hvb  		   : uniquement comp (entreprises)"
+    	echo "   hva 		   : uniquement comp (entreprises)"
     	echo "   lv  		   : comp, indiv (particuliers) ou all (tous)"
-    	echo "  <id_centrale>      : Identifiant de la centrale (optionnel)"
+    	echo "  <id_centrale>      : Identifiant de la centrale (option)"
     	echo "Options interdites :"
     	echo " hvb all, hvb indiv, hva all, hva indiv"
     	echo "  -h                 : Affiche cette aide (prioritaire)"
@@ -20,7 +20,7 @@ aide(){
 if [[ "$#" -lt 3 ] || [[ "$*" == *"-h"* ]]; then #si l'option -h est demandé quelque soit l'endroit ou si le nombre d'argument est inferieur a trois 
 	aide
 	exit 1
-}
+fi
 
 chemin_fichier=$1
 station=$2
@@ -28,21 +28,21 @@ conso=$3
 id_centrale=$4
 
 if [ ! -f "$chemin_fichier" ]; then
-    echo "Erreur : Le fichier CSV '$chemin_fichier' est n'existe pas."
+    echo "Erreur : fichier introuvable"
     aide
     echo "temps : 0.0sec"
     exit 2
 fi
 
 if [[ "$station" != "hvb" && "$station" != "hva" && "$station" != "lv" ]]; then
-    echo "Erreur : La station saisie n'existe pas. Valeurs possibles : hvb, hva, lv."
+    echo "Erreur : La station n'existe pas. Valeurs possibles : hvb, hva, lv."
     aide
     echo "temps : 0.0sec"
     exit 3
 fi
 
 if [[ "$conso" != "comp" && "$conso" != "indiv" && "$conso" != "all" ]]; then
-    echo "Erreur : Le consommateur saisie n'existe pas. Valeurs possibles : comp, indiv, all."
+    echo "Erreur : Le consommateur n'existe pas. Valeurs possibles : comp, indiv, all."
     aide
     exit 4
 fi
@@ -74,31 +74,58 @@ if [ ! -f "$executable" ]; then #si l'executable n'existe pas
 fi
 
 if [ ! -d "tmp" ]; then
-    echo "Le dossier 'tmp' n'existe pas. Création du dossier."
+    echo "Le dossier 'tmp' n'existe pas. Création."
     mkdir -p tmp
 else
-    echo "Le dossier 'tmp' existe déjà. Il sera vidé."
+    echo "Le dossier 'tmp' existe déjà. On le vide"
     rm -rf tmp/* #surprime tout ce qui est a l'interieur  
 fi
 
 # Vérification et création du dossier graphs
 if [ ! -d "graphs" ]; then
-    echo "Le dossier 'graphs' n'existe pas. Création du dossier."
+    echo "Le dossier 'graphs' n'existe pas. Création."
     mkdir -p graphs
 else
     echo "Le dossier 'graphs' existe déjà."
 fi
 
-debut = $(date + %s)
+debut=$(date + %s) #sert a connaitre le temps d'execution : ici ça prend l'heure du début
 
-#faire le scrpit avec le filtrage
+#filtrage
+gcc -o projet projet.c
+
+if [[ "$2" == "hvb" ]] && [[ "$3" == "comp" ]]; then # si l'utilisateur rentre hvb comp
+	
+
+fi 
+
+if [[ "$2" == "hva" ]] && [[ "$3" == "comp" ]]; then #si l'utilisateur ecrit hva comp
+	
+fi 
+
+if [[ "$2" == "lv" ]] && [[ "$3" == "comp" ]]; then # si l'utilisateur rentre lv comp
+	
+
+fi 
+
+if [[ "$2" == "lv" ]] && [[ "$3" == "indiv" ]]; then # si l'utilisateur rentre lv indiv
+	
+
+fi 
+
+if [[ "$2" == "lv" ]] && [[ "$3" == "all" ]]; then # si l'utilisateur rentre lv all
+	
+
+fi 
+
+# il manque le cas ou l'utilsateur met une option avec l'identifiant de la central
+
+fin=$(date + %s) #prend l'heure a la fin du filtrage
 
 
-fin = $(date + %s)
+duree=$(debut - fin) # on fait la différence pour avoir le temps total d'execution
+echo "temps : $duree sec" # on affiche ce temps
 
-
-duree = $(debut - fin)
-echo "temps : $dureesec"
 
 
 
