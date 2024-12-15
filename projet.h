09659fdc7bcfa23dfb5873acf88555438f8aabe9
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-12-15 12:38:26 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -4,11 +4,6 @@
 typedef struct station {
     char type_station[10];
     int id; //identifiant d’une centrale électrique
-   /* int id_B; //identifiant d’une station HV-B
-    int id_A; //identifiant d’une station HV-A
-    int id_LV; //identifiant d’un poste LV
-    int id_e; //identifiant d’une entreprise (consommateur)
-    int id_p; //identifiant d’un particulier (consommateur) */
     int load; //quantité d’énergie consommée par le consommateur final entreprise ou particulier
     int capacite;//quantité d’énergie produite par une centrale
     int som_conso; // sommes de consomation
