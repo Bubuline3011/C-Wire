086e005e7833c6ee77ed5213073f6a6559501cb7
Bubuline3011 <167123750+Bubuline3011@users.noreply.github.com>
2024-12-19 19:36:03 +0100

    Add files via upload

--- projet.h
+++ projet.h
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <errno.h>
+#define MAX_LIGNE 256 // Taille maximale d'une ligne
 
 // Définition de la structure 'Station' qui représente une station
-typedef struct {
+typedef struct { //on cree cette structure pour essayer d'optimiser 
     int id; //identifiant d’une centrale électrique 
-    int load; //quantité d’énergie consommée par le consommateur final entreprise ou particulier
+    int conso; //quantité d’énergie consommée par le consommateur final entreprise ou particulier
     int capacite;//quantité d’énergie produite par une centrale
-    int som_conso; // sommes de consomation
 }Station; 
 
 // Définition de la structure 'AVL' pour l'arbre AVL
 typedef struct AVL{
     Station * station;
-    struct AVL * fg;
-    struct AVL * fd;
-    int equilibre;
+    struct AVL * fg; //pointeur vers fils gauche
+    struct AVL * fd; // pointeur vers fils droit
+    int equilibre; // facteur d'equilibre
 }AVL;
 
 // Définition d'un type pavl pour un pointeur vers un arbre AVL
@@ -26,17 +27,15 @@ int max(int a, int b);
 int min(int a, int b);
 int min3(int a, int b, int c);
 int max3(int a, int b, int c);
-pavl creerAvl(Station * a);
+pavl creerAvl(int id, int capacite, int conso);
 pavl rotationGauche(pavl a);
 pavl rotationDroite(pavl a);
 pavl doubleRotationDroite(pavl a);
 pavl doubleRotationGauche(pavl a);
 pavl equilibreAVL(pavl a);
-pavl insertionAVL(pavl a, Station * e, int * h);
-pavl suppMinAVL(pavl a, int * h, int * pe);
-pavl suppressionAVL(pavl a, int * h, Station * e);
-void sommeConso(pavl a);
-void ecrireFichierSortie(const char *nomFichier, pavl a);
-void ecrireFichierSortieRec(pavl a, FILE *f);
-void libérer_noeud_station(pavl noeud);
-pavl lireFichierCSV(const char *nomFichier, pavl racine);
+pavl insertionAVL(pavl a, int id, int capacite, int conso, int * h);
+int recherche(pavl noeud, int id);
+void supprimer_AVL(pavl a);
+void recuperer_info_csv();
+void parcoursInfixe(pavl a, FILE* f);
+void ecrire_ds_fichier_result_tmp(pavl a);
