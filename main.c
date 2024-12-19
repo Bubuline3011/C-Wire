7850752ae5c69bdf620e7edee71b51570ca42855
Bubuline3011 <167123750+Bubuline3011@users.noreply.github.com>
2024-12-19 19:36:49 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,11 @@
 #include "projet.h"
 
 // Programme principal
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <fichier_csv_entree> <fichier_csv_sortie>\n", argv[0]);
-        exit(6);
-    }
-
-    const char *fichierEntree = argv[1];
-    const char *fichierSortie = argv[2];
-
-    pavl racine = NULL;
-    racine = lireFichierCSV(fichierEntree, racine);
-
-    sommeConso(racine);
-    ecrireFichierSortie(fichierSortie, racine);
-
-    libérer_noeud_station(racine);
-
-    return 0;
+int main(){
+	pavl a = NULL;
+	recuperer_info_csv(&a);
+	ecrire_ds_fichier_result_tmp(a);
+	supprimer_AVL(a);
+	return 0;
 }
 
