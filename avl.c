35e242a94f87689910c3d2db04e51fbf3e56a0d9
Bubuline3011 <167123750+Bubuline3011@users.noreply.github.com>
2024-12-19 19:38:21 +0100

    Add files via upload

--- avl.c
+++ avl.c
@@ -1,51 +1,46 @@
 #include "projet.h"
 
 // Fonction pour créer un arbre AVL avec une station donnée
-pavl creerAvl(Station * a){
-	if(a == NULL){
-		printf("Erreur");
+pavl creerAvl(int id, int capacite, int conso){
+	if(id < 0 || capacite < 0 || conso < 0){
+		printf("Erreur : les parametres fournies sont incorrecte");
 		exit(1);
 	}
 	pavl noeud = malloc(sizeof(AVL)); // Allocation dynamique pour un nouvel arbre AVL
 	if(noeud==NULL){
+		printf("Erreur d'allocation memoire lors de la creation de l'AVL");
    		exit(2); //Allocation échouée
    	}
 	 // Attribution de tous les champ a notre nouveau noeud de l'AVL
-	noeud->station = a;
+	noeud->station->id = id;
+	noeud->station->conso = conso;
+	noeud->station->capacite = capacite;
 	noeud->fg = NULL; // Le sous-arbre gauche est initialement NULL
 	noeud->fd = NULL; // Le sous-arbre droit est initialement NULL
 	noeud->equilibre = 0; //L'équilibre initial de l'arbre est 0 (arbre vide)
     	return noeud;
 }
 
-Station * creerStation(int id,int capacite,int load,int centrale_id){
-	Station *new=malloc(sizeof(Station));
-	if(new==NULL){
-		exit(3);
-	}
-	new->id=id;
-	new->capacite=capacite;
-	new->load=load;
-	new->id=centrale_id;
-	new->som_conso=0;
-	return new;
-}
+
 
 // Fonction d'insertion d'une station dans l'arbre AVL avec rééquilibrage
-pavl insertionAVL(pavl a, Station * e, int * h) {
+pavl insertionAVL(pavl a, int id, int capacite, int conso, int * h) {
     	if (a == NULL) {
        		*h = 1;                      // Si l'arbre est vide, on crée un nouvel arbre
-        	return creerAvl(e);           // Créer un nouvel arbre avec la station
+        	return creerAvl(id, capacite, conso);           // Créer un nouvel arbre avec la station
     	} 
-    	else if (e->id < a->station->id) {         // Si la station est inférieure à la station actuelle
-        	a->fg = insertionAVL(a->fg, e, h); // Insertion récursive dans le sous-arbre gauche
+    	else if (id < a->station->id) {         // Si la station est inférieure à la station actuelle
+        	a->fg = insertionAVL(a->fg, id, capacite, conso, h); // Insertion récursive dans le sous-arbre gauche
         	*h = -(*h);                  // Inverser l'état de hauteur
     	} 
-    	else if (e->id > a->station->id) {         // Si la station est supérieure à la station actuelle
-        	a->fd = insertionAVL(a->fd, e, h); // Insertion récursive dans le sous-arbre droit
+    	else if (id > a->station->id) {         // Si la station est supérieure à la station actuelle
+        	a->fd = insertionAVL(a->fd, id, capacite, conso, h); // Insertion récursive dans le sous-arbre droit
     	} 
-    	else {                            // Si la station est déjà présente, on ne l'insère pas
-    		//a->station->load = a->station->load + a->load; //changer la ligne 
+    	else {                            // si c'est le meme identifiant alors on somme la consommation et on met a jour la capacite
+    		if(a->station->capacite==0 && capacite !=0){ // 
+    			a->station->capacite = a->station->capacite + capacite;
+    		}
+    		a->station->conso = a->station->conso + conso; 
         	*h = 0;
         	return a;
     	}
@@ -76,15 +71,6 @@ int recherche(pavl noeud, int id) {
         	return recherche(noeud->fd, id);
     	} 
 }
-
-void parcoursInfixe(pavl a){
-	if(a != NULL){
-		parcoursInfixe(a->fg);
-		printf("(Id : %d, Capacite : %d, Load : %d, Equilibre : %d, Somme : %d)", a->station->id, a->station->capacite, a->station->load, a->equilibre, a->station->som_conso);
-		parcoursInfixe(a->fd);
-	}
-}
-
 
 // Fonction de rotation à gauche pour rééquilibrer l'arbre AVL
 pavl rotationGauche(pavl a) {  	
@@ -150,90 +136,17 @@ pavl equilibreAVL(pavl a){
     return a;
 }
 
-pavl suppMinAVL(pavl a, int * h, int * pe){
-    	pavl tmp;
-    	if(a->fg == NULL){
-        	*pe = a->station->id;
-        	*h = -1;
-        	tmp = a;
-        	a = a->fd;
-        	free(tmp);
-        	return a;
-    	}
-    	else {
-        	a->fg = suppMinAVL(a->fg, h, pe);
-   	}
-    	if(*h != 0){
-        	a->equilibre = a->equilibre + *h;
-        	a = equilibreAVL(a);
-        	if(a->equilibre == 0){
-            		*h = 0;
-        	}
-        	else {
-           	 	*h = 1;
-        	}
-    	}
-    	return a;
-}
-
-pavl suppressionAVL(pavl a, int * h, Station * e){
-	pavl tmp;
-    	if(a == NULL){
-        	*h = 0;
-        	return a;
-    	}
-    	else if(e->id < a->station->id){
-        	a->fg = suppressionAVL(a->fg, h, e);
-        	*h = -*h;
-    	}
-    	else if(e->id > a->station->id){
-        	a->fd = suppressionAVL(a->fd,h, e);
-    	}
-    	else if(a->fd != NULL){
-        	a->fd = suppMinAVL(a->fd, h, &a->station->id);
-    	}
-    	else {
-        	tmp = a;
-        	a = a->fg;
-        	*h = -1;
-        	free(tmp);
-        	return a;
-    	}
-    	if(*h != 0){
-        	a->equilibre = a->equilibre + *h;
-        	a = equilibreAVL(a);
-        	if(a->equilibre == 0){
-            		*h = 0;
-        	}
-        	else {
-            		*h = 1;
-        	}
-    	}
-    	return a;
-}
-
-void sommeConso(pavl a) {
-    	if (a == NULL){ 
-    		return;
-    	}
-    	sommeConso(a->fg);
-    	sommeConso(a->fd);
-    	a->station->som_conso = a->station->load;
-    	if (a->fg != NULL){
-    		a->station->som_conso += a->fg->station->som_conso;
-    	}
-    	if (a->fd != NULL){ 
-    		a->station->som_conso += a->fd->station->som_conso;
-    	}
-}
-
-// Libération de la mémoire de l'AVL
-void libérer_noeud_station(pavl noeud) {
-    	if (noeud == NULL){// Si le nœud est NULL, on ne fait rien
-    		return;
+void supprimer_AVL(pavl a){
+	if(a != NULL){
+		if(a->fg != NULL){
+			supprimer_AVL(a->fg);
+		}
+		if(a->fd != NULL){
+			supprimer_AVL(a->fd);
+		}
+		free(a->station);
+		free(a);
 	}
-    	libérer_noeud_station(noeud->fg); // Libérer le sous-arbre gauche
-    	libérer_noeud_station(noeud->fd);// Libérer le sous-arbre droit
-    	free(noeud->station);
-	free(noeud);	// Libérer le nœud courant
 }
+
+
